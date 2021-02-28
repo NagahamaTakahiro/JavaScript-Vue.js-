5f21405163409ebf6ccd95ea0a834d59435d7f35
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-02-28 10:52:56 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const word = leapYear;
+const yearA = 2020;
+const yearB = 2021;
+
+console.log(yearA);
+console.log(yearB);
+
+if (yearA % 400 == 0) {
+  console.log("leapYear");
+} else if (yearB % 400 == 0) {
+  console.log("leapYear");
+} 
+
+
